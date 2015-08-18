57dd60a00aa057854363cce491a69b40b933009e
CHELSEA <chaika.design@gmail.com>
2015-08-18 14:30:01 +0900

    js async 読み込みのテスト refs https://teratail.com/questions/14600

---
+++ js/js/writeImage.js
@@ -0,0 +1,5 @@
+var d = document;
+var url = 'http://cdn-ak.b.st-hatena.com/entryimage/236933222-origin-1419307828.jpg';
+var img = d.createElement('img');
+img.src = url;
+d.getElementsByTagName('body').item(0).appendChild(img);
