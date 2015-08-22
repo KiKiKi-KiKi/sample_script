9105a01f28ce7273b80d6a8600ca9cce039bd2a9
Chaika <chaika.design@gmail.com>
2015-08-22 15:59:58 +0900

    bowerのCSS, jsをコンパイルするgulpタスク

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,64 @@
 var gulp = require("gulp"),
+    plumber = require("gulp-plumber"),
+    concat = require("gulp-concat"),
+    bower = require('main-bower-files'),
+    rename = require('gulp-rename'),
+    gulpFilter = require('gulp-filter'),
+    // CSS
     sass = require("gulp-sass"),
     //compass = require('gulp-compass'),
     autoprefixer = require("gulp-autoprefixer"),
-    plumber = require("gulp-plumber"),
-    concat = require("gulp-concat"),
-    minifyCss = require("gulp-minify-css");
+    minifyCss = require("gulp-minify-css"),
+    less = require('gulp-less'),
+    // JS
+    uglify = require("gulp-uglify");
+
+// bower CSS
+gulp.task('bowerCSS', function() {
+  var cssDir = 'assets/css/',
+      cssFilter  = gulpFilter('**/*.css', {restore: true}),
+      scssFilter = gulpFilter('**/*.scss', {restore: true}),
+      sassFilter = gulpFilter('**/*.sass', {restore: true}),
+      lessFilter = gulpFilter('**/*.less', {restore: true});
+  return gulp.src( bower({
+      paths: {
+        bowerJson: 'bower.json'
+      }
+    }) )
+    .pipe( lessFilter )
+    .pipe( less() )
+    .pipe( concat('_bundle.css') )
+    .pipe( gulp.dest(cssDir) )
+    .pipe( minifyCss() )
+    .pipe( rename({
+      extname: '.min.css'
+    }) )
+    .pipe( gulp.dest(cssDir) )
+    .pipe( lessFilter.restore );
+});
+// bower JS
+gulp.task('bowerJS', function() {
+  var jsDir = 'assets/js/',
+      jsFilter = gulpFilter('**/*.js', {restore: true});
+  return gulp.src( bower({
+    paths: {
+      bowerJson: 'bower.json'
+    }
+  }) )
+  .pipe( jsFilter )
+  .pipe( concat('_bundle.js') )
+  .pipe( gulp.dest(jsDir) )
+  .pipe( uglify({
+    preserveComments: 'some'
+  }) )
+  .pipe( rename({
+    extname: '.min.js'
+  }) )
+  .pipe( gulp.dest(jsDir) )
+  .pipe( jsFilter.restore );
+});
+
+gulp.task('bower', ['bowerCSS', 'bowerJS']);
 
 gulp.task("compass", function() {
   return gulp.src('assets/scss/*.scss')
@@ -20,7 +74,7 @@ gulp.task("cssmin", function(){
       'assets/css/slick.css',
       'assets/css/font-awesome.css'
     ])
-    .pipe(concat('all.css'))
+    .pipe(concat('_all.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('assets/css/'));
 });
