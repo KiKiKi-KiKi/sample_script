ea501e5e6640ac7d94ba719c33d9d3a69bb366f1
CHAIKA <chaika.design@gmail.com>
2015-10-19 13:49:48 +0900

    php ajax refs. https://teratail.com/questions/18031

---
+++ php/a.php
@@ -0,0 +1,11 @@
+<?php /* https://teratail.com/questions/18031 */ ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>A.php</h1>
+    <?php include_once('./b.php');?>
+  </body>
+ </html>
