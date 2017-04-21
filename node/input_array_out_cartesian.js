b5e12526e2e0516f42392efe4c590e34cb97df1d
Chaika <chaika.design@gmail.com>
2017-04-21 23:02:11 +0900

    FIX 乱数初期化ミスを修正

--- node/input_array_out_cartesian.js
+++ node/input_array_out_cartesian.js
@@ -9,12 +9,12 @@ process.stdin.on('data', function(chunk) {
 });
 process.stdin.on('end', function() {
   var d = input.toString().split('\n')[0].split(" ").map((i)=> i-0),
-      seed = (d[1]-d[0])+d[0],
+      seed = (d[1]-d[0]),
       arr = [];
 
   // ランダムな配列を作成
   for(let i=d[0]; i<d[1]; i+=1) {
-    arr.push( Math.floor( Math.random()*seed ) );
+    arr.push( Math.floor( Math.random()*seed + d[0] ) );
   }
 
   console.log(arr);
