4d0f71eda652e32219a44e79585c60d1916c366d
KiKiKi <chaika.design@gmail.com>
2018-10-22 18:26:51 +0900

    Promise.all(), Promise.race()

--- node/es2015_Promise.js
+++ node/es2015_Promise.js
@@ -49,3 +49,86 @@ hi('星宮いちご').then((res) => {
   // 共通のエラー処理
   console.log(error)
 })
+
+// ## 並行処理
+// ### Promise.all(iterable)
+// ref. https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+/*
+> Promise.all(iterable)メソッドは、
+> iterable のすべての Promise が完了したときや、
+> iterable な引数に Promise が含まれないときに、完了した Promiseを返します。
+> 最初に失敗した Promise の失敗理由とともに失敗を返します。
+*/
+Promise.all([
+  hi('神前美月'),
+  hi('藤堂ユリカ'),
+  hi('一ノ瀬かえで')
+])
+.then((res) => {
+  // すべての非同期処理が完了したら、結果がArrayで返される
+  console.log(res)
+})
+.catch((error) => console.log(error))
+// [ 'Hi, 神前美月!', 'Hi, 藤堂ユリカ!', 'Hi, 一ノ瀬かえで!' ]
+
+var p1 = Promise.resolve(3)
+var p2 = 1337
+var p3 = new Promise((resolve, reject) => setTimeout(reject, 100, 'Foo'))
+var p4 = Promise.reject('Bar')
+
+Promise.all([p1, p2, p3, p4])
+.then(
+  (res) => console.log(res),
+  // 1つでもエラーがあると、最初にエラーになったものが返る
+  (error) => console.log('e =>', error)
+)
+// e => Bar
+
+// catch を使った場合も、最初にエラーになったものがキャッチされる
+Promise.all([p1, p2, p3])
+.then((res) => {
+  console.log(res)
+})
+.catch((error) => {
+  console.log('e =>', error)
+})
+// e => Foo
+
+// ### Promise.race(iterable)
+/*
+> 引数 iterable に含まれる Promise のうち、
+> 最初に完了 (成功または失敗) した Promise の値を返す Promise を返します。
+*/
+Promise.race([
+  hi('風沢そら'),
+  hi('冴草きい'),
+  hi('音城セイラ'),
+  hi('姫里マリア')
+])
+.then((res) => {
+  // 最初に完了した処理の返り値がそのまま渡される
+  console.log(res)
+})
+.catch((err) => {
+  console.log(err)
+})
+// Hi, 風沢そら!
+
+var pError = Promise.reject('Error!')
+
+Promise.race([
+  pError,
+  hi('冴草きい'),
+  hi('音城セイラ'),
+  hi('姫里マリア')
+])
+.then((res) => {
+  console.log(res)
+})
+.catch((err) => {
+  // 最初にエラーが返るとエラー処理に流れて終了する
+  console.log('Error:', err)
+})
+// Error: Promise.race Error!
+
+// => Promise.race() は`resolve`, `reject`関係なしに最初に返された処理だけが行われて終了する
