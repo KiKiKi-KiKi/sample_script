f0c5c46ed031af3f19eebb79cf658f534db225b6
KiKiKi <chaika.design@gmail.com>
2018-10-27 17:03:33 +0900

    Promise.all & Promise.race 途中で失敗するものがあっても、呼び出されている非同期処理は実行される

--- node/es2015_Promise.js
+++ node/es2015_Promise.js
@@ -7,6 +7,7 @@ refs
 - https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Using_promises
 - https://qiita.com/koki_cheese/items/c559da338a3d307c9d88
 - https://qiita.com/toshihirock/items/e49b66f8685a8510bd76
+- https://qiita.com/naoto_koyama/items/7234d133f0d5f87532db
 */
 
 function hi(name) {
@@ -132,3 +133,80 @@ Promise.race([
 // Error: Promise.race Error!
 
 // => Promise.race() は`resolve`, `reject`関係なしに最初に返された処理だけが行われて終了する
+
+console.log('---------')
+
+const task1 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('>> TASK-1')
+      resolve('task1')
+    }, 20)
+  })
+}
+
+const task2 = () => {
+  return new Promise((resolve, rejecy) => {
+    setTimeout(() => {
+      console.log('>> TASK-2')
+      resolve('task2')
+    }, 5)
+  })
+}
+
+const task3 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('>> TASK-3')
+      reject('REJECT TASK3')
+    }, 10)
+  })
+}
+
+setTimeout(() => {
+  console.log('--- Promise.all ---')
+  Promise.all([task1(), task2(), task3()])
+    .then((res) => {
+      console.log('>>> Promise.all' ,res)
+    })
+    .catch((error) => {
+      console.log('>>> Promise.all ERROR')
+    })
+  /*
+  >> TASK-2
+  >> TASK-3
+  >>> Promise.all ERROR
+  >> TASK-1
+  => 非同期の処理はストップされない
+  */
+}, 500)
+
+/*
+Promise.all([task1(), task2(), task3()])
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((error) => {
+    console.log('>>> Promise.all ERROR')
+    process.exit(1)
+  })
+*/
+// process.exit(1) を使うとエラーの場合その場でプロセスが止まる
+
+setTimeout(() => {
+  console.log('--- Promise.race ---')
+  Promise.race([task1(), task3()])
+    .then((res) => {
+      console.log('>>> Promise.race' ,res)
+    })
+    .catch((error) => {
+      console.log('>>> Promise.race ERROR')
+    })
+
+  /*
+  >> TASK-3
+  >>> Promise.race ERROR
+  >> TASK-1
+  */
+  // Promise.race も同様に非同期の処理はストップしない
+}, 1000)
