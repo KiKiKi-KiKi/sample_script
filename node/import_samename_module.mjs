fce33c8988f4268581c8529e84c81014ef012eae
KiKiKi <chaika.design@gmail.com>
2018-12-15 18:43:59 +0900

    node ESmodule TEST

--- node/import_samename_module.mjs
+++ node/import_samename_module.mjs
@@ -2,12 +2,15 @@ import {cube} from './modules/my_moduleA';
 import {cube as rectangular} from './modules/my_moduleB';
 // * を使う時は {} で囲むとエラーになる
 import * as mc from './modules/my_moduleC';
+import foo from './modules/my_moduleD';
 
 console.log( 'moduleA cube 5 = ', cube(5) );
 console.log( 'moduleB cube x5 y5 x10 = ', rectangular(5, 5, 10) );
 console.log( 'moduleC cube w5 h10 = ', mc.cube(5, 10) );
 
 console.log( mc.hi('星宮いちご') );
+
+console.log( foo.name )
 
 /*
 note
@@ -42,17 +45,6 @@ $ node main.mjs
 SyntaxError: Unexpected token
 ```
 
-`-experimental-modules`オプションを使わないとダメっぽい
-```javascript
-// main.mjs
-import {cube} from './modules/my_moduleA';
-```
-```sh
-$ node -experimental-modules main.mjs
-(node:49180) ExperimentalWarning: The ESM module loader is experimental.
-moduleA cube 5 =  125
-```
-
 インポートする関数名が`{}`で囲まれてないとエラー
 ```javascript
 // main.mjs
@@ -63,5 +55,24 @@ $ node -experimental-modules main.mjs
 import cube from './modules/my_moduleA';
        ^^^^
 SyntaxError: The requested module './modules/my_moduleA' does not provide an export named 'default'
+```
+
+`--experimental-modules`オプションを使わないとダメっぽい
+```javascript
+// main.mjs
+import {cube} from './modules/my_moduleA';
+```
+```sh
+$ node --experimental-modules main.mjs
+(node:49180) ExperimentalWarning: The ESM module loader is experimental.
+moduleA cube 5 =  125
+```
+
+`--experimental-modules`オプションはファイルの前に無いとダメ
+```sh
+$ node main.mjs --experimental-modules
+(function (exports, require, module, __filename, __dirname) { import {cube} from './modules/my_moduleA';
+                                                                     ^
+SyntaxError: Unexpected token
 ```
 */
