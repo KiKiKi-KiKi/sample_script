62132b4018eb1c712949be4f27aeebe061982c54
KiKiKi <chaika.design@gmail.com>
2019-03-26 22:51:33 +0900

    Set a function to Object in ES2015

--- node/es2015_object.js
+++ node/es2015_object.js
@@ -3,14 +3,42 @@
 let cute = '星宮いちご';
 let cool = '霧矢あおい';
 let sexy = '紫吹蘭';
-const soleil_es5 = {
+
+// ES5
+let soleil_es5 = {
   cute: cute,
   cool: cool,
   sexy: sexy,
 };
 console.log( soleil_es5 );
 // { cute: '星宮いちご', cool: '霧矢あおい', sexy: '紫吹蘭' }
-const soleil_es2015 = { cute, cool, sexy };
+
+// ES2015
+let soleil_es2015 = { cute, cool, sexy };
 console.log( soleil_es2015 );
 // { cute: '星宮いちご', cool: '霧矢あおい', sexy: '紫吹蘭' }
 
+// set function
+// ES5
+soleil_es5 = {
+  cute: cute,
+  cool: cool,
+  sexy: sexy,
+  toString: function() {
+    return `ES5 Soleil ${this.cute}, ${this.cool}, ${this.sexy}`;
+  }
+};
+console.log( soleil_es5.toString() );
+// ES5 Soleil 星宮いちご, 霧矢あおい, 紫吹蘭
+
+// ES2015
+soleil_es2015 = {
+  cute,
+  cool,
+  sexy,
+  toString() {
+    return `ES2015 Soleil ${this.cute}, ${this.cool}, ${this.sexy}`;
+  }
+};
+console.log( soleil_es2015.toString() );
+// ES2015 Soleil 星宮いちご, 霧矢あおい, 紫吹蘭
