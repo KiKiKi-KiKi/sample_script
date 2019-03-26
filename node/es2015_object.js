4fd1672cd66d7dcb304e5d6080f1b12eb1abd8dd
KiKiKi <chaika.design@gmail.com>
2019-03-27 00:07:28 +0900

    ES2015  can set Object key via variable like a {[variable]: value}

--- node/es2015_object.js
+++ node/es2015_object.js
@@ -42,3 +42,29 @@ soleil_es2015 = {
 };
 console.log( soleil_es2015.toString() );
 // ES2015 Soleil 星宮いちご, 霧矢あおい, 紫吹蘭
+
+// Set Object key via Variable
+const key = 'pop';
+const value = '夏樹みくる';
+
+// ES5
+const es5Obj = {};
+es5Obj[key] = value;
+console.log('ES5', es5Obj);
+// ES5 { pop: '夏樹みくる' }
+
+// ES2015
+const es2015Obj = {
+  [key]: value,
+};
+console.log('ES2015', es2015Obj);
+// ES2015 { pop: '夏樹みくる' }
+
+let i = 0;
+const myObj = {
+  [`index-${++i}`]: i * i,
+  [`index-${++i}`]: i * i,
+  [`index-${++i}`]: i * i,
+};
+console.log( myObj);
+// { 'index-1': 1, 'index-2': 4, 'index-3': 9 }
