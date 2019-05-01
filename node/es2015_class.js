67bf06ea67ac745f314951eaac6e53136277309f
KiKiKi <chaika.design@gmail.com>
2019-05-02 00:14:26 +0900

    super method in class

--- node/es2015_class.js
+++ node/es2015_class.js
@@ -19,3 +19,30 @@ const idol = {
 };
 
 console.log( idol.to_s() );
+
+// class
+class Student {
+  constructor() {
+    this.school = 'スターライト学園';
+  }
+  static say_school() {
+    return this.school;
+  }
+};
+
+class Idol extends Student {
+  constructor(_name) {
+    super();
+    this.name = _name;
+  }
+  say() {
+    return `${this.name} 所属: ${this.school}`;
+  }
+  static my_school() {
+    return super.say_school();
+  }
+};
+
+const hoshiiya = new Idol('星宮いちご');
+console.log( hoshiiya.say() );
+console.log( Idol.my_school() );
