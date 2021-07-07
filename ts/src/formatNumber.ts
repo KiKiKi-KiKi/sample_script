35788f49b14f1ab83b693756cfaaaa1749a0c3c5
KiKiKi <chaika.design@gmail.com>
2021-07-07 16:39:40 +0900

    Format number.
    cf.
    - https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
    - https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
    - https://qumeru.com/magazine/426
    - https://qiita.com/zawascript/items/922b5db574ef2b126069
    - https://pisuke-code.com/javascript-way-to-get-decimal-part/

---
+++ ts/src/formatNumber.ts
@@ -0,0 +1,128 @@
+export {};
+
+console.log('>>> Format Number');
+
+const num1: number = 1234567890;
+const num2: number = 1234567890.1235;
+const num3: number = -1234567;
+const num4: number = -1234567.89054321;
+const data = [num1, num2, num3, num4];
+
+console.log('Number.prototype.toLocaleString');
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+const localeString = (n: number): string => {
+  return n.toLocaleString();
+};
+
+console.log(data.map((num) => localeString(num)));
+// => '1,234,567,890'
+// => '1,234,567,890.124'
+// => '-1,234,567'
+// => '-1,234,567.891'
+
+// maximumSignificantDigits option を使用することで丸める桁数を指定できる
+console.log(num2.toLocaleString(undefined, { maximumSignificantDigits: 20 }));
+// => '1,234,567,890.1235'
+
+console.log('>>> Intl.NumberFormat');
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+const numberFormat =
+  (locales?: string | string[], options?: Intl.NumberFormatOptions) =>
+  (n: number): string => {
+    return new Intl.NumberFormat(locales, options).format(n);
+  };
+
+const formatEN = numberFormat();
+console.log(data.map((num) => formatEN(num)));
+// => '1,234,567,890'
+// => '1,234,567,890.124'
+// => '-1,234,567',
+// => -1,234,567.891'
+
+const formatJP = numberFormat('ja-JP');
+console.log(data.map((num) => formatJP(num)));
+// => '1,234,567,890'
+// => '1,234,567,890.124'
+// => '-1,234,567',
+// => -1,234,567.891'
+
+const formatDE = numberFormat('de-DE');
+console.log(data.map((num) => formatDE(num)));
+// => '1.234.567.890',
+// => '1.234.567.890,124'
+// => '-1.234.567',
+// => '-1.234.567,891'
+
+console.log('>>> 正規表現');
+
+const splitIntAndDecimal = (num: number): [number, number] => {
+  const numStr = String(num);
+  const decimalPointIndex = numStr.indexOf('.');
+  if (decimalPointIndex < 0) {
+    return [num, 0];
+  }
+
+  const initPart = Math.floor(num);
+  const decimalPartStr = `0.${numStr.slice(decimalPointIndex + 1)}`;
+  const decimalPart = parseFloat(decimalPartStr);
+
+  return [initPart, num < 0 ? -decimalPart : decimalPart];
+};
+
+const splitIntAndDecimalBySplit = (num: number): [number, number] => {
+  const isNegative = num < 0;
+  const numStr = String(Math.abs(num));
+  const splitNumStr = numStr.split('.');
+  const initPartStr = splitNumStr[0];
+  const decimalPartStr = splitNumStr.length > 1 ? splitNumStr[1] : '0';
+  const initAndDecimalStr: [string, string] = [initPartStr, decimalPartStr];
+
+  return initAndDecimalStr.map((n, i) => {
+    const num = i === 0 ? Number(n) : parseFloat(`0.${n}`);
+    return isNegative ? -num : num;
+  }) as [number, number];
+};
+
+console.log('>>> split number');
+console.log(data.map((num) => splitIntAndDecimal(num)));
+console.log(data.map((num) => splitIntAndDecimalBySplit(num)));
+
+console.log('>>> split with replace');
+const formatRegExp = (num: number): string => {
+  const numStrings = String(num).split('.');
+  return [
+    numStrings[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,'),
+    ...numStrings.slice(1),
+  ].join('.');
+};
+console.log(data.map((num) => formatRegExp(num)));
+// => '1,234,567,890',
+// => '1,234,567,890.1235',
+// => '-1,234,567',
+// => '-1,234,567.89054321'
+
+console.log('>>> 再帰関数');
+
+const separate = (numStr: string, separator: string = ','): string => {
+  const length = numStr.length;
+
+  if (length > 3) {
+    const splitIndex = length - 3;
+    return [
+      separate(numStr.substring(0, splitIndex), separator),
+      numStr.substring(splitIndex),
+    ].join(separator);
+  }
+
+  return numStr;
+};
+
+const formatNumber = (num: number): string => {
+  const numStr = String(num).split('.');
+  return [separate(numStr[0], ' '), ...numStr.slice(1)].join('.');
+};
+console.log(data.map((num) => formatNumber(num)));
+// => '1 234 567 890',
+// =>  '1 234 567 890.1235',
+// =>  '-1 234 567',
+// =>  '-1 234 567.89054321'
